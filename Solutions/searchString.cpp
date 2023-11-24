f27417ef4c680507d2a864d943a7f8a4db851475
nasifay <nasifayc11@gmail.com>
2023-11-24 22:24:24 +0000

    Update File

--- Solutions/searchString.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-  /*Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.*/
-  //.../
-}
